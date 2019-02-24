c08d782a558c01da3a38b887481bbab0e57884a6
Alex Kalmuk <alexkalmuk@gmail.com>
2019-02-24 21:23:35 +0300

    tests: controllability.m: Check for bode plot and stability

--- tests/contollability.m
+++ tests/contollability.m
@@ -21,6 +21,30 @@ else
 end
 
 sys_ss = ss(A,B,C,0);
-ts_ss = tf(sys_ss);
+tf_ss = tf(sys_ss);
 
-bodeplot(sys_ss)
+fprintf('State space system:\n');
+sys_ss
+
+fprintf('Transfer function:\n');
+tf_ss
+
+fprintf('Check if system is stable (Bode plot make sence only if the system is stable):');
+isstable(sys_ss)
+
+sys_ss_from_tf = ss(tf_ss);
+
+figure('Name', 'Bode plots', 'Position', [10 500 600 400], 'Color', 'w');
+figure(1);
+bodeplot(tf_ss, 'r', sys_ss_from_tf, 'y--');
+
+figure('Name', 'Nyquist plot', 'Position', [800 500 600 400], 'Color', 'w');
+figure(2);
+nyquist(sys_ss);
+
+% Just feed sin to the system and see what happen
+t = 0:0.1:20;
+u = sin(0.5*t);
+figure('Name', 'Simulating tf response', 'Position', [650 10 600 400], 'Color', 'w');
+figure(3);
+lsim(tf_ss,u,t);
