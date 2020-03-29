4ed50373a12a8856812688e3b3dbdf1439830aca
Alex Kalmuk <alexkalmuk@gmail.com>
2020-03-29 20:09:07 +0300

    mpc: Extend simple LQR

--- misc/mpc/mpc.py
+++ misc/mpc/mpc.py
@@ -11,14 +11,19 @@ sys = signal.StateSpace(A, B, C, D)
 
 T = 100
 t = np.arange(T)
-x0 = np.array([0.5, 0.5])
-u = np.ones((T, 2))
-x = np.ones((T, 2))
+x0 = np.array([0, 0])
+r = np.array([2, 0])
+u = np.zeros((T, 2))
+x = np.zeros((T, 2))
 
 x[0] = x0
+u[0] = np.linalg.inv(B) @ r
+# x = (A - B * K) * x
+K = np.linalg.inv(B) @ (A - np.identity(2))
 
 for i in range(0, T - 1):
 	x[i + 1] = A @ x[i] + B @ u[i]
+	u[i + 1] = -K @ x[i + 1]
 
 print(x[:,0])
 
