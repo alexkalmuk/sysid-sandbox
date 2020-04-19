9631a4d8e3e97f1c6013aaf74902e290a9ddcf69
Alex Kalmuk <alexkalmuk@gmail.com>
2020-04-19 19:57:08 +0300

    lqr poles

--- misc/lqr/lqr.py
+++ misc/lqr/lqr.py
@@ -6,14 +6,20 @@ from control.matlab import *
 A = np.matrix([[-0.0285, -0.0014], [-0.0371, -0.1476]])
 B = np.matrix([[-0.0850, 0.0238], [0.0802, 0.4462]])
 
+C = ctrb(A, B)
+print("Controllability matrix =", C)
+print("Rank =", np.linalg.matrix_rank(C))
+
+ss = StateSpace(A,B, np.eye(2), np.zeros((2,2)))
+p = pole(ss)
+print("poles =", p)
+
 # Start with a diagonal weighting
 Q = np.diag([1, 1])
 R = np.diag([1, 1])
 K, S, E = lqr(A, B, Q, R)
 
 print("K =", K)
-print("S =", S)
-print("E =", E)
 
 T = 100
 t = np.arange(T)
